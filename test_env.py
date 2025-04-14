31face2fac31e61e27b5a5d3bde2bcc212da3a1b
sakshiagarwala0206 <sakshiagarwala0206@gmail.com>
2025-04-14 22:53:58 +0530

    some more changes

--- test_env.py
+++
@@ -1,9 +0,0 @@
-from pendulum_env import InvertedPendulumEnv
-env = InvertedPendulumEnv(render=True)
-obs = env.reset()
-for _ in range(1000):
-    action = env.action_space.sample()
-    obs, reward, done, _ = env.step(action)
-    if done:
-        break
-env.close()
